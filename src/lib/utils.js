5cd7850d434e37ee973c6c45829e1b63801f60ed
David Sullivan <treatkor@gmail.com>
2024-08-11 05:10:53 -0700

    use luxon Duration

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 
 export const getTotalDuration = (obj) => {
@@ -9,13 +9,17 @@ export const getTotalDuration = (obj) => {
 		})
 		.reduce((total, seconds) => total + seconds, 0);
 
-	const totalDuration = DateTime.fromSeconds(totalSeconds).toFormat('mm:ss');
+	if (totalSeconds == 0) return 0;
 
-	if (obj.length) {
-		return obj.length > 1 ? totalDuration : obj[0].duration;
-	} else {
-		return '00:00';
-	}
+	const dur = Duration.fromMillis(totalSeconds*1000);
+	// const totalDuration = DateTime.fromSeconds(totalSeconds).toFormat('mm:ss');
+	const totalDuration = dur.toFormat('hh:mm:ss');
+	const tmpArr = totalDuration.split(':')
+	const obby = {hours: tmpArr[0], minutes: tmpArr[1], seconds: tmpArr[2]}
+	const foox = Duration.fromObject(obby)
+	// console.log('foox: ', foox);
+	// const formattedDuration = foo[0] + 'hrs ' + foo[1] + 'mins ' + foo[2] + 'secs '
+	return foox.toHuman({unitDisplay: 'short', listStyle:'narrow'});
 };
 
 export const sortByKey = (arr, key, ascdesc) => {
@@ -26,17 +30,18 @@ export const sortByKey = (arr, key, ascdesc) => {
 	}
 }
 
-export const formatCSV = (obj) => {
-	let rows = `name, album, duration, tuning\n`;
+const formatCSV = (obj) => {
+	let rows = `name,album,duration,tuning\n`;
 	obj.forEach((s) => {
-		rows += `${s.name}, ${s.album}, ${s.duration}, ${s.tuning}\n`;
+		rows += `${s.name},${s.album},${s.duration},${s.tuning}\n`;
 	});
 	return rows
 }
 
 export const readCSV = (data) => {
-	const lines = data.split('\n');
+	const lines = data.split('\n').filter(x => x);
 	const keys = lines[0].split(',');
+
 	const output = lines.slice(1).map((line) => {
 		const values = line.split(',');
 		const obj = {};
@@ -45,19 +50,47 @@ export const readCSV = (data) => {
 		});
 		return obj;
 	});
+	console.log('readCSV: output', output)
 	return output
 }
 
-export const writeFile = (output, mimetype='text/csv') => {
+export const writeFile = (output, filename='untitled.csv', mimetype='text/csv') => {
 	const foo = formatCSV(output);
 	const output2 = foo
 	const blob = new Blob([output2], { type: mimetype });
 	const url = URL.createObjectURL(blob);
 	const a = document.createElement('a');
 	a.href = url;
-	a.download = 'untitled.csv';
+	a.download = filename;
 	a.click();
 };
+
+// export const readFile = async (event) => {
+// 	const file = event.target.files[0];
+// 	console.log('readFile() ', file)
+// 	const reader = new FileReader();
+
+// 	reader.onload = async(event) => {
+// 		const data = event.target.result;
+// 		let tmp = readCSV(data);
+// 		console.log('tmp: ');
+// 		console.log(tmp);
+// 		if (tmp) {
+// 			return {
+// 				success:true,
+// 				data:tmp,
+// 				message: `loaded ${file.name}`
+// 			}
+// 		} else {
+// 			return {
+// 				success: false,
+// 				data: null,
+// 				message: `ERROR: expected CSV file ${file.name}`
+// 			};
+// 		}
+// 	}
+// 	reader.readAsText(file);
+// }
 
 // export const readFile = (event) => {
 // 	const file = event.target.files[0];
